ec378052949158058de1006a7ad0d6e2ccadca93
niho <n@maxdoom.com>
2024-03-11 19:35:47 +0100

    initial commit

---
+++ html/main/public_html/index.php
@@ -0,0 +1,9 @@
+<h1>docker test suit</h1>
+<ul>
+<li><a href="https://localhost.direct:8456" target="_blank">PHP 5.6</a></li>
+<li><a href="https://localhost.direct:8474" target="_blank">PHP 7.4</a></li>
+<li><a href="https://localhost.direct:8480" target="_blank">PHP 8.0</a></li>
+<li><a href="https://localhost.direct:8481" target="_blank">PHP 8.1</a></li>
+<li><a href="https://localhost.direct:8482" target="_blank">PHP 8.2</a></li>
+<li><a href="https://localhost.direct:8483" target="_blank">PHP 8.3</a></li>
+</ul>
